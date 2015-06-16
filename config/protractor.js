ac1d6ef26fd8b510394bef63f9d3678ab73aa95b
eduardogml <eduardogml.webmaster@gmail.com>
2015-06-16 09:11:15 -0300

    soucer labs

--- config/protractor.js
+++ config/protractor.js
@@ -1,6 +1,16 @@
+var config = require('./config')();
+
 exports.config = {
-  specs: ['../test/e2e/**/*.js'],
-  onPrepare: function() {
-    browser.driver.get('http://localhost:3000');
-  }
+	sauceUser : config.sauceUser, 
+  	sauceKey : config.sauceKey, 
+  	capabilities : {
+	    'name': config.sauceTestName,
+	    'browserName': 'chrome',
+	    'tunnel-identifier': config.travisJobNumber,
+	    'build': config.travisBuild
+	},
+  	specs: ['../test/e2e/**/*.js'],
+  	onPrepare: function() {
+	    browser.driver.get('http://localhost:3000');
+  	}
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var http = require('http');
 var app  = require('./config/express')();
+var config = require('./config/config')();
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Applq Express Nodejs Server v1.0.0: escutando na porta ' + app.get('port'));
