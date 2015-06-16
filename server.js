c6f05ec5d3b7d5f0b0767bef4666442ac9675d7b
eduardogml <eduardogml.webmaster@gmail.com>
2015-06-16 09:51:31 -0300

    inicio - configuracao ambiente producao

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var app  = require('./config/express')();
 var config = require('./config/config')();
 
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Applq Express Nodejs Server v1.0.0: escutando na porta ' + app.get('port'));
+http.createServer(app).listen(config.port, config.address, function(){
+	console.log('Applq Express Nodejs Server v1.0.0: ' 
+	+ config.address + ' (' + config.env + ') escutando na porta ' + config.port);
 });
